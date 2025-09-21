dcab7b43853c9b75715b89ca90da4ecd2acec83a
Amit Suryavanshi <amit.sury1102@gmail.com>
2025-09-21 16:04:21 +0530

    Add files via upload

---
+++ Local-RAG/local-rag-chatbot - Copy.py
@@ -0,0 +1,120 @@
+# This script use the knowledge store built from documents to answer user queries using RAG approach.
+# It first tries to find the answer in the knowledge base and if not found, it falls back to general LLM.
+# Make sure that you've built the knowledge store by executing the indexing script first.
+# Required packages: chromadb, ollama, streamlit, subprocess
+
+# Import packages
+import chromadb
+import ollama
+import subprocess
+import streamlit as st
+
+# Function to retrieve answer using RAG approach
+def retrieve_answer(query, embedding_model, llm_model, collection):
+    # Get embedding for the query
+    query_embedding = ollama.embed(model=embedding_model, input=query)['embeddings'][0]
+    
+    # Retrieve top-2 relevant documents from Chroma DB
+    results = collection.query(
+        query_embeddings=[query_embedding],
+        n_results=2,
+        include=['documents', 'distances', 'metadatas']
+    )
+
+    similarity_threshold=0.7 # Max distance to consider a chunk's relevance
+    
+    # Extract documents, distances and metadata from the results
+    documents = results.get('documents')
+    distances = results.get('distances')
+    metadatas = results.get('metadatas')
+    
+    context = []
+    source_docs = []
+    # check if documents is not None i.e. the query on chromadb has returned some results
+    # and documents[0] exists i.e. the first query’s results are non-empty
+    if documents and documents[0]:
+        for doc, dist, meta in zip(documents[0], distances[0], metadatas[0]):
+            #Build the context by selecting only those documents that are similar enough to the user's query.
+            #This avoids passing irrelevant chunks to LLM.
+            if dist <= similarity_threshold:
+                context.append(doc)
+                source_docs.append(meta.get('source', 'N/A'))
+    
+    # Join sources into a single string (if any found)
+    if source_docs:
+        source_docs = ",".join(source_docs)            
+    
+    #Above check ensure that if distance of the chunk from the knowledge store is greater
+    # than the similarity_threshold then context will be empty
+    answer = generate_answer(query, context, llm_model)
+    
+    # return answer with source citations if context is found
+    return f"Source Docs: {source_docs} \n\n {answer}" if source_docs else answer
+
+# Function to generate answer using LLM given context and question
+def generate_answer(query, context, llm_model):
+    
+    # Prepare the instruction to be passed to LLM
+    prompt = f"""Answer the question below naturally and directly. 
+        Only use the context if it is helpful. Do not include extra explanation unless necessary.
+    
+    Context: {context}
+    Question: {query}
+    Answer:
+    """
+    
+    #generate prompt using ollama
+    result = subprocess.run(
+        ["ollama", "run", llm_model, prompt],
+        capture_output=True,
+        text=True,
+        encoding="utf-8",    # Force UTF-8 decoding
+        errors="ignore"      # Optional: ignore any invalid characters
+    )
+    return result.stdout.strip()
+   
+
+######################### Initialize Chroma DB and models ###########
+# Persistent Chroma DB client
+path=r"D:\RAG\chroma_db"   #Absolute Path where ChromaDB is stored
+crdb_client = chromadb.PersistentClient(path)
+
+# Get the collection, make sure the collection name matches the one used in indexing script
+collection = crdb_client.get_collection("kb_collection")
+
+# Embedding model
+embedding_model = 'hf.co/CompendiumLabs/bge-base-en-v1.5-gguf'
+#LLM model
+llm_model = 'hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF'
+######################### Init END #########################
+
+
+######################### Streamlit UI Code BEGIN ###########
+
+st.set_page_config(page_title="RAG Chatbot", page_icon="\U0001F916") #\U0001F916 is unicode () for 🤖
+st.title("\U0001F916 I'm RAG Bot")
+
+# Store chat history
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+# Display history
+for role, msg in st.session_state["messages"]:
+    st.chat_message(role).write(msg)
+
+# Chat input
+if prompt := st.chat_input("Type your question..."):
+    # Show user message
+    st.chat_message("user").write(prompt)
+    st.session_state["messages"].append(("user", prompt))
+
+    bot_response = []
+    with st.spinner("Thinking..."):
+        answer = retrieve_answer(prompt, embedding_model, llm_model, collection)
+        
+    # Show bot message
+    st.chat_message("assistant").write(answer)
+    st.session_state["messages"].append(("assistant", answer))
+
+######################### Streamlit UI Code END #########################
+
---
+++ Local-RAG/local-rag-indexing.py
@@ -0,0 +1,106 @@
+# This script builds the knowledge store from documents by generating embeddings from it using EMBEDDING_MODEL.
+# Generated embeddings are stored in persistent Chroma DB which will act as vector store for RAG information retrieval. 
+# Make sure to run this script once before running the Streamlit UI script.
+# Required packages: chromadb, ollama, python-docx, PyPDF2
+
+import chromadb     #Chroma DB for vector store
+import ollama       # Ollama client for embeddings
+import os           # For file handling
+import PyPDF2      # For reading PDF documents
+from docx import Document # For reading Word documents
+
+# Function to load all word (.docx) and pdfs (.pdf) from a folder
+# Enhance this function to load documents from other sources like databases, Sharepoint, web etc.
+def load_documents(folder_path):
+
+    docs = []
+    doc_ids = []
+    filenames = []
+
+    # iterate through all files in the folder
+    for i, filename in enumerate(os.listdir(folder_path)):
+        file_path = os.path.join(folder_path, filename)
+        text = ""
+        # Handle word documents with .docx extension
+        if filename.endswith(".docx"):
+            print(f"Processing word doc: \"{filename}\"")
+            doc = Document(file_path)
+            text = "\n".join([p.text for p in doc.paragraphs if p.text.strip() != ""])
+        # Handle PDF documents with .pdf extension    
+        elif filename.endswith(".pdf"):
+            print(f"Processing PDF file: \"{filename}\"")
+            with open(file_path, "rb") as f:
+                reader = PyPDF2.PdfReader(f)
+                for page in reader.pages:
+                    page_text = page.extract_text()
+                    if page_text:
+                        text += page_text + "\n"
+        else:
+            print(f"Skipping unsupported file: {filename}") 
+            continue  # skip unsupported file types
+
+        if text.strip():  # only add if there's content
+            docs.append(text)
+            doc_ids.append(str(i))
+            filenames.append(filename)
+    
+    print("Reading documents completed!")
+    return docs, doc_ids, filenames
+
+# add a chunking mechanism if documents are large
+def chunk_text(text, chunk_size=500, overlap=50):
+    words = text.split()
+    chunks = []
+    for i in range(0, len(words), chunk_size - overlap):
+        chunk = " ".join(words[i:i + chunk_size])
+        chunks.append(chunk)
+    return chunks
+
+# Function to generate embeddings and add to Chroma
+def add_docs_to_chroma(docs, doc_ids, filenames, collection, embedding_model):
+
+    chunk_size=500
+    overlap=50
+    for doc, doc_id, filename in zip(docs, doc_ids, filenames):
+        # Chunk the document if it's too large
+        chunks = chunk_text(doc, chunk_size, overlap)
+        for i, chunk in enumerate(chunks):
+            emb = ollama.embed(model=embedding_model, input=chunk)["embeddings"][0]
+            collection.add(
+                documents=[doc],
+                embeddings=[emb],
+                ids=[doc_id],
+                metadatas=[{"source": filename}]  # for citations
+            )
+        print(f"Added document ID {doc_id} from file \"{filename}\".")
+
+######################### Knowledge Store Handling BEGIN ###############
+
+# Get folder from where the knowledge store shall be build
+print("\n\nStarting the process to build the Knowledge Store...")
+folder_path = input("\nEnter the path of the folder to pickup the documents from (e.g. D:\\Docs): ")
+path = input("\nEnter New Folder path where knowldege store shall be created (e.g. D:\\knowledge-store): ")
+
+# Load documents
+print(f"\nReading documents (.docx, .pdf) from folder: {folder_path}...")
+docs, doc_ids, filenames = load_documents(folder_path)
+
+# Persistent Chroma DB client
+print(f"\nCreating Persistent Chromdb at location: {path}...")
+crdb_client = chromadb.PersistentClient(path)
+
+# Create or get collection. Collections are where your embeddings, documents, and metadata will be stored
+#  'get_or_create_collection` avoids creating a new collection every time
+collection = crdb_client.get_or_create_collection("kb_collection")
+
+# Embedding model
+embedding_model = 'hf.co/CompendiumLabs/bge-base-en-v1.5-gguf'
+
+# Add documents to Chroma DB
+print("\nGenerating embeddings and loading them to Chroma DB...")
+add_docs_to_chroma(docs, doc_ids, filenames, collection, embedding_model)
+
+# Persist the Chroma DB to disk
+print(f"Persistent Chroma DB ready. All documents are added to ChromaDB at location: {path}")
+print("Process Successfully completed!")
+
