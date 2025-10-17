1ad4167a4d70cdd4a0541ad27a3cf727bbae00ed
Amit Suryavanshi <amit.sury1102@gmail.com>
2025-10-17 19:14:11 +0530

    Refactor graph.py to improve code structure

--- GitMate-AI/app/graph/graph.py
+++ GitMate-AI/app/graph/graph.py
@@ -35,7 +35,7 @@ def call_llm(state: AgentState, llm):
     - To get overview of existing files in Main branch: use FileOverview
 	- To fetch a list of the repository's issues: use GetIssues
     - To fetch a list of all files in a specified directory: Use GtflsfrmDrctry  
-	- To read the contents of a file in pr: use github_fileoverview_tool 	
+	- To read the contents of a file in pr: use github_prfileoverview_tool 	
 	- To add comment on Issue: Use github_issuecomment_tool
     - To get all the open pull requests on repo: use github_allpullreqs_tool
     - To get details/summary of a specific PR: use github_detailpr_tool
@@ -112,6 +112,7 @@ def execute_graph(app,conversation_history,user_id):
     return last_message.content
 
 ######################## END  ###################################
+
 
 
 
