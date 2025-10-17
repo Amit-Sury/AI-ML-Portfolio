d57f88b7d706b2fd6608997d2321a49848fb2490
Amit Suryavanshi <amit.sury1102@gmail.com>
2025-10-17 11:47:26 +0530

    Add main.py to start the bot UI

---
+++ GitMate-AI/app/main.py
@@ -0,0 +1,7 @@
+################# Import Dependeinces ###################
+#my packages
+from ui import start_bot_ui 
+######################## END  ##########################
+
+# This runs automatically when Streamlit run command is executed with main.py
+start_bot_ui()
