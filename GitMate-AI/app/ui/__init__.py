01ff82052708881d99d7245c644366c7b4284625
Amit Suryavanshi <amit.sury1102@gmail.com>
2025-10-17 12:09:51 +0530

    Add files via upload

---
+++ GitMate-AI/app/ui/__init__.py
@@ -0,0 +1,5 @@
+# This makes 'UI' a Python package
+# You can also choose to expose specific functions directly here
+from .helper_functions import initialize_app, write_convo
+from .streamlit_ui import start_bot_ui
+
