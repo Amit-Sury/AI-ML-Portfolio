2059da516d18726e06d3fdfac742f8de7d39ec49
Amit Suryavanshi <amit.sury1102@gmail.com>
2025-10-19 22:56:56 +0530

    Add files via upload

--- GitMate-AI/app/tools/load_env.py
+++ GitMate-AI/app/tools/load_env.py
@@ -9,10 +9,8 @@ from tools import LOG
 def loadenvfile(st):
     """This function load the .env file"""
 
-    if not load_dotenv(dotenv_path="./.env", override=True):
-        return -1
-
-    
+    load_dotenv()  
+        
     #make directories if not already exists
     #os.makedirs(os.path.dirname(os.environ["HISTORY_PATH"]), exist_ok=True)
     #os.makedirs(os.path.dirname(os.environ["LOG_PATH"]), exist_ok=True)
@@ -31,18 +29,15 @@ def loadenvfile(st):
 
     #Update private key
     key_path = os.getenv("GITHUB_APP_PRIVATE_KEY")  # path from .env
-    LOG(f"Github private key path:{key_path}")
-    
+        
     #streamlit rerun() causes issue if GITHUB_APP_PRIVATE_KEY is already inflated
     #and code attempts to open the key_path. Below check makes it safe
     if key_path and os.path.isfile(key_path): 
-        LOG(f"Reading github private key from path:{key_path}")
         with open(key_path, "r") as f:
             private_key = f.read()
         
         os.environ["GITHUB_APP_PRIVATE_KEY"] = private_key
         LOG("✅Reading github private key completed")
-        return 1
-    else:
-        LOG(f"❌Github private key not found in path:{key_path}")
-        return -1
+        
+    
+    return 1
