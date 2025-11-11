da8a2b59122807b02feadb93e6cdd49c45785dd9
Amit Suryavanshi <amit.sury1102@gmail.com>
2025-11-11 12:32:47 +0530

    Add files via upload

--- GitMate-AI/app/graph/init_llm.py
+++ GitMate-AI/app/graph/init_llm.py
@@ -21,13 +21,15 @@ def get_llm(tools):
     
     llmtype = int(os.environ["LLM_TYPE"])
     model_id = os.environ["LLM_MODEL_ID"]
+    base_url = os.environ["BASE_URL"]
+    aws_region = os.environ["AWS_REGION"]
 
     if not (1 <= llmtype <= 3):
-        LOG("❌Invalid LLM Type, it must be 1-3")
+        LOG("❌Invalid LLM_TYPE in .env, it must be 1-3")
         return -1
     
     if not model_id.strip(): 
-        LOG("❌Invalid Model_ID.")
+        LOG("❌LLM_MODEL_ID is not found .env.")
         return -1
 
     LOG(f"LLM type={llmtype}, model_id={model_id}")
@@ -35,7 +37,12 @@ def get_llm(tools):
     if llmtype == 1: #Use ollama llm
         try:
             LOG("Initializing Ollama LLM...")
-            llm = ChatOllama(model = model_id).bind_tools(tools=tools) 
+            if not base_url.strip(): 
+                LOG("❌BASE_URL for Ollama not found in .env file")
+                LOG("💡For direct script use: http://localhost:11434")
+                LOG("💡For docker use: http://host.docker.internal:11434")                
+                return -1
+            llm = ChatOllama(model = model_id, base_url=base_url).bind_tools(tools=tools) 
             llm.invoke("hi")
             LOG(f"✅Ollama LLM {model_id} initialization Succeeded!")
             return llm
@@ -46,9 +53,12 @@ def get_llm(tools):
             return -1
 
     elif llmtype == 2: #Use AWS bedrock llm
-        try: 
-            LOG("Initializing AWS Bedrock LLM...")
-            bedrock_agent = boto3.client("bedrock-runtime", region_name ="us-east-1")
+        try:
+            if not aws_region.strip(): 
+                LOG("❌AWS_REGION is not found in .env file")                
+                return -1 
+            LOG(f"Initializing AWS Bedrock LLM for region {aws_region}...")
+            bedrock_agent = boto3.client("bedrock-runtime", region_name = aws_region)
             llm_create = ChatBedrockConverse(
                 model=model_id,
                 temperature=0,
