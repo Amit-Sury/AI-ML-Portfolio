7d04035c72a796f822fe3983faa1c3b50d865c1c
Amit Suryavanshi <amit.sury1102@gmail.com>
2025-11-28 20:09:08 +0530

    Update imports in tools package initialization

--- GitRepoAssist-AI/app/tools/__init__.py
+++ GitRepoAssist-AI/app/tools/__init__.py
@@ -1,8 +1,9 @@
 # This makes 'tools' a Python package
 # You can also choose to expose specific functions directly here
-from .contextvars import set_contextvar_userid, get_contextvar_userid
+from .contextvars_util import set_contextvar_userid, get_contextvar_userid
 from .log_handler import LOG, pretty_print_messages
 from .github_tools import *
 from .youtube_tools import get_youtubesearch
 from .load_env import loadenvfile
 from .github_token_handler import GitTokenHandler
+
